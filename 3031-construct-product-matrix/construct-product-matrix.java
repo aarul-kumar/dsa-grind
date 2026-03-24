ad64fa23dfd1971d8aea28493161887b239d8a08
Aarul Kumar <aarulk0110@gmail.com>
2026-03-25 04:25:25 +0530

    Time: 11 ms (57.56%) | Memory: 101 MB (44.39%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.java
+++ 3031-construct-product-matrix/construct-product-matrix.java
@@ -3,39 +3,24 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int mod = 12345;
-        int prefixProd[][] = new int[n][m];
-        int suffixProd[][] = new int[n][m];
-        
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(i==0 && j==0) {
-                    prefixProd[i][j] = 1;
-                } else if(j==0) {
-                    prefixProd[i][j] = (int)((long)prefixProd[i-1][m-1]*grid[i-1][m-1]%mod);
-                } else {
-                    prefixProd[i][j] = (int)((long)prefixProd[i][j-1]*grid[i][j-1]%mod);
-                }
-            }
+
+        int res[][] = new int[n][m];
+        long prefix = 1;
+        for(int i=0; i<n*m; i++) {
+            int r = i/m;
+            int c = i%m;
+            res[r][c] = (int)prefix;
+            prefix = (prefix*grid[r][c])%mod;
         }
 
-        for(int i=n-1; i>=0; i--) {
-            for(int j=m-1; j>=0; j--) {
-                if(i==n-1 && j==m-1) {
-                    suffixProd[i][j] = 1;
-                } else if(j==m-1) {
-                    suffixProd[i][j] = (int)((long)suffixProd[i+1][0]*grid[i+1][0]%mod);
-                } else {
-                    suffixProd[i][j] = (int)((long)suffixProd[i][j+1]*grid[i][j+1]%mod);
-                }
-            }
+        long suffix = 1;
+        for(int i=n*m-1; i>=0; i--) {
+            int r = i/m;
+            int c = i%m;
+            res[r][c] = (int)((res[r][c]*suffix)%mod);
+            suffix = (suffix*grid[r][c])%mod;
         }
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                grid[i][j] = (int)((long)prefixProd[i][j]*suffixProd[i][j]%mod);
-            }
-        }
-
-        return grid;
+        return res;
     }
 }
