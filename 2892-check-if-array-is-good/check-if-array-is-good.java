b6f84481b9f0550bf55a2e2f59496a523f11c721
Aarul Kumar <aarulk0110@gmail.com>
2026-05-14 11:29:07 +0530

    Time: 6 ms (15.24%) | Memory: 44.9 MB (52.00%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isGood(int[] nums) {
         Arrays.sort(nums);
-        int max = nums[nums.length-1];
-        if(nums.length != max+1) {
+        int n = nums.length;
+        int max = nums[n-1];
+        if(n != max+1) {
             return false;
         }
         int j = 1;
-        for(int i=0; i<nums.length-1; i++) {
+        for(int i=0; i<n-1; i++) {
             if(nums[i]!=j) {
                 return false;
             }
             j++;
         }
-        if(nums[nums.length-1]==max) {
+        if(nums[n-1]==max) {
             return true;
         }
         return false;
