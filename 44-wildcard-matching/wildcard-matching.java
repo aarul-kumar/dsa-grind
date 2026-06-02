cf4624d5ee3290dde4cf699bc70677d6569704ea
Aarul Kumar <aarulk0110@gmail.com>
2026-06-03 01:52:47 +0530

    Time: 37 ms (49.00%) | Memory: 47.2 MB (64.52%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -6,24 +6,19 @@ class Solution {
 
         dp[0][0]=true;
 
-        for(int i=1; i<n+1; i++){
-            dp[i][0]=false;
-        }
-
-        for(int j=1; j<m+1; j++){
+        for(int j=1; j<=m; j++){
             if(p.charAt(j-1)=='*'){
                 dp[0][j]=dp[0][j-1];
             }
         }
 
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<m+1; j++){
-                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?'){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                char ch=p.charAt(j-1);
+                if(s.charAt(i-1)==ch || ch=='?'){
                     dp[i][j]=dp[i-1][j-1];
-                }else if(p.charAt(j-1)=='*'){
+                }else if(ch=='*'){
                     dp[i][j]=dp[i][j-1] || dp[i-1][j];
-                }else{
-                    dp[i][j]=false;
                 }
             }
         }
