6a6b16811d0803f509ddcf0384b42038634b05fb
Aarul Kumar <aarulk0110@gmail.com>
2026-06-04 08:59:49 +0530

    Time: 15 ms (81.44%) | Memory: 47.1 MB (19.32%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int dp[][] = new int[coins.length+1][amount+1];
+        for(int i=0; i<dp.length; i++) {
+            dp[i][0]=0;
+        }
+        for(int j=1; j<dp[0].length; j++) {
+            dp[0][j]=Integer.MAX_VALUE-1;
+        }
+        for(int i=1; i<dp.length; i++) {
+            for(int j=1; j<dp[0].length; j++) {
+                if(coins[i-1]<=j) {
+                    dp[i][j]=Math.min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
+                } else {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        if(dp[dp.length-1][dp[0].length-1]==Integer.MAX_VALUE-1){
+            return -1;
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+}
