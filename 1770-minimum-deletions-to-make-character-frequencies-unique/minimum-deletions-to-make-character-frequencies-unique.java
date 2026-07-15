0c83132e31706314b2471271a6d58844423b21ff
Aarul Kumar <aarulk0110@gmail.com>
2026-07-16 03:17:29 +0530

    Time: 59 ms (6.36%) | Memory: 47.1 MB (97.25%) - LeetSync

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minDeletions(String s) {
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(int i=0; i<s.length(); i++) {
+            hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+        }
+        int steps = 0;
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int val:hm.values()) {
+            arr.add(val);
+        }
+        arr.sort(Comparator.reverseOrder());
+        HashSet<Integer> hs = new HashSet<>();
+        int step = 0;
+        for(int i=0; i<arr.size(); i++) {
+            int curr = arr.get(i);
+            while(hs.contains(curr) && curr>0) {
+                curr--;
+                step++;
+            }
+            hs.add(curr);
+        }
+        return step;
+    }
+}
