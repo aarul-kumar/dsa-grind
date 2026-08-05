0850724f9236d446195c6734402a1d2c4819f872
Aarul Kumar <aarulk0110@gmail.com>
2026-08-05 11:59:37 +0530

    Time: 4 ms (94.30%) | Memory: 46.6 MB (91.45%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -14,7 +14,7 @@ class Solution {
         for(int i=0; i<grid.length; i++) {
             int src = grid[i][1];
             int dest = grid[i][0];
-            graph[src].add(new Edge(src,dest));
+            graph[src].add(new Edge(src, dest));
         }
     }
     public boolean isCycle(ArrayList<Edge> graph[]) {
@@ -22,61 +22,59 @@ class Solution {
         boolean stack[] = new boolean[graph.length];
         for(int i=0; i<graph.length; i++) {
             if(!vis[i]) {
-                if(isCycleUtil(graph,vis,stack,i)) {
+                if(isCycleUtil(graph, i, vis, stack)) {
                     return true;
                 }
             }
         }
         return false;
     }
-    public boolean isCycleUtil(ArrayList<Edge> graph[], boolean vis[], boolean stack[], int curr) {
+    public boolean isCycleUtil(ArrayList<Edge> graph[], int curr, boolean vis[], boolean stack[]) {
         vis[curr] = true;
         stack[curr] = true;
         for(int i=0; i<graph[curr].size(); i++) {
             Edge e = graph[curr].get(i);
             if(stack[e.dest]) {
                 return true;
-            } else if(!vis[e.dest] && isCycleUtil(graph,vis,stack,e.dest)) {
+            }
+            if(!vis[e.dest] && isCycleUtil(graph, e.dest, vis, stack)) {
                 return true;
             }
         }
         stack[curr] = false;
         return false;
     }
-    ArrayList<Integer> arr = new ArrayList<>();
+    Stack<Integer> s = new Stack<>();
     public void topSort(ArrayList<Edge> graph[]) {
         boolean vis[] = new boolean[graph.length];
-        Stack<Integer> s = new Stack<>();
         for(int i=0; i<graph.length; i++) {
             if(!vis[i]) {
-                topSortUtil(graph,vis,s,i);
+                topSortUtil(graph, s, i, vis);
             }
         }
-        while(!s.isEmpty()) {
-            arr.add(s.pop());
-        }
     }
-    public void topSortUtil(ArrayList<Edge> graph[], boolean vis[], Stack<Integer> s, int curr) {
+    public void topSortUtil(ArrayList<Edge> graph[], Stack<Integer> s, int curr, boolean vis[]) {
         vis[curr] = true;
         for(int i=0; i<graph[curr].size(); i++) {
             Edge e = graph[curr].get(i);
             if(!vis[e.dest]) {
-                topSortUtil(graph,vis,s,e.dest);
+                topSortUtil(graph, s, e.dest, vis);
             }
         }
         s.push(curr);
     }
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<Edge> graph[] = new ArrayList[numCourses];
-        createGraph(graph,prerequisites);
+        createGraph(graph, prerequisites);
         if(isCycle(graph)) {
             return new int[]{};
         }
         topSort(graph);
-        int ans[] = new int[arr.size()];
-        for(int i=0; i<arr.size(); i++) {
-            ans[i] = arr.get(i);
+        int arr[] = new int[s.size()];
+        int i = 0;
+        while(!s.isEmpty()) {
+            arr[i++] = s.pop();
         }
-        return ans;
+        return arr;
     }
 }
