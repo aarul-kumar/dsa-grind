4512461b333862893f6b65834447ac826a7ff21a
Aarul Kumar <aarulk0110@gmail.com>
2026-08-05 17:05:40 +0530

    Time: 1413 ms (5.00%) | Memory: 206.6 MB (5.00%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,53 +1,51 @@
 class Solution {
-    public class Edge implements Comparable<Edge> {
+    class Edge implements Comparable<Edge> {
         int src;
         int dest;
         int wt;
-        public Edge(int s, int d, int w) {
-            this.src=s;
-            this.dest=d;
-            this.wt=w;
+        Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
         }
         @Override
         public int compareTo(Edge e) {
             return this.wt-e.wt;
         }
     }
-
-    public void createGraph(int[][] points, ArrayList<Edge> graph[]) {
+    public void createGraph(ArrayList<Edge> graph[], int grid[][]) {
         for(int i=0; i<graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
-        for(int i=0; i<points.length; i++) {
-            for(int j=0; j<points.length; j++) {
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid.length; j++) {
                 if(i!=j) {
-                    int src=i;
-                    int dest=j;
-                    int wt=Math.abs(points[i][0]-points[j][0])+Math.abs(points[i][1]-points[j][1]);
-                    graph[src].add(new Edge(src,dest,wt));
+                    int src = i;
+                    int dest = j;
+                    int wt = Math.abs(grid[i][0]-grid[j][0])+Math.abs(grid[i][1]-grid[j][1]);
+                    graph[src].add(new Edge(src, dest, wt));
                 }
             }
         }
     }
-
     public int minCostConnectPoints(int[][] points) {
         ArrayList<Edge> graph[] = new ArrayList[points.length];
-        createGraph(points,graph);
-        boolean vis[] = new boolean[graph.length];
+        createGraph(graph, points);
+        boolean vis[] = new boolean[points.length];
         PriorityQueue<Edge> pq = new PriorityQueue<>();
         pq.add(new Edge(0,0,0));
-        int minCost = 0;
+        int min = 0;
         while(!pq.isEmpty()) {
             Edge curr = pq.remove();
             if(!vis[curr.src]) {
-                vis[curr.src]=true;
-                minCost+=curr.wt;
+                vis[curr.src] = true;
+                min += curr.wt;
                 for(int i=0; i<graph[curr.src].size(); i++) {
                     Edge e = graph[curr.src].get(i);
-                    pq.add(new Edge(e.dest,0,e.wt));
+                    pq.add(new Edge(e.dest,e.src,e.wt));
                 }
             }
         }
-        return minCost;
+        return min;
     }
 }
